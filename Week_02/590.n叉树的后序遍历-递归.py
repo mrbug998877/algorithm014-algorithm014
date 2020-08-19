11830f73bcec6c71badf5c71bbca48e964a90f9c
mrbug998877 <gaojunling.gjl@gmail.com>
2020-08-19 12:16:17 +0800

    更新

---
+++ Week_02/590.n叉树的后序遍历-递归.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=590 lang=python3
+#
+# [590] N叉树的后序遍历
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        res=[]
+        def recursion (root):
+            res.append(root.val)
+            for child in root.children:
+                recursion(child)
+        recursion(root)
+        return res
+# @lc code=end
+
