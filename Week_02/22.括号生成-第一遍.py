c98db0d4960214adb891ca17f7803f8bd58ab2bb
mrbug998877 <gaojunling.gjl@gmail.com>
2020-08-26 10:37:00 +0800

    括号生成

---
+++ Week_02/22.括号生成-第一遍.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=22 lang=python3
+#
+# [22] 括号生成
+#
+
+# @lc code=start
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.ans = []
+        self.generator(n,n,"")
+        return self.ans
+    def generator(self,left, right, curr):
+        #terminator
+        if left == right == 0:
+            self.ans.append(curr)
+            return
+        #current level 
+        #drill down
+        if left:
+            self.generator(left-1,right,curr+'(')
+        if right > left:
+            self.generator(left,right-1,curr+')')
+        #
+
+# @lc code=end
+
