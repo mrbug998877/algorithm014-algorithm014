fed6a75cb095e44681e12720bafd8136fd600a40
mrbug998877 <gaojunling.gjl@gmail.com>
2020-09-04 17:01:37 +0800

    friday

---
+++ Week_04/200.岛屿数量-第一遍.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=200 lang=python3
+#
+# [200] 岛屿数量
+#
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        """
+        i: index of row;
+        j index of column;
+        """
+        def sink(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[i]) and grid[i][j] == '1':
+                grid[i][j] = '0'
+                map(sink, (i+1, i-1, i, i), (j, j, j+1, j-1))
+                return 1
+            return 0
+        return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
+# @lc code=end
+
---
+++ Week_04/200.岛屿数量-第二遍.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=200 lang=python3
+#
+# [200] 岛屿数量
+#
+
+# @lc code=start
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        
+        count = 0
+        for i in range(len(grid)):
+            
+# @lc code=end
+
